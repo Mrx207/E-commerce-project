62e33bb72be8c83e34580f0607ae81ed0af8fc0e
Rushikesh Deshmukh <rushikesh.deshmukh2@ing.com>
2023-07-03 22:46:04 +0530

    navbar sidebar and footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+import {
+  Home,
+  PrivateRoute,
+  ProductsPage,
+  SingleProductPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  AboutPage,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <AboutPage />
+        </Route>
+        <Route exact path="/cart">
+          <CartPage />
+        </Route>
+        <Route exact path="/products">
+          <ProductsPage />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProductPage />} />
+        <Route exact path="/checkout">
+          <CheckoutPage />
+        </Route>
+        <Route exact path="*">
+          <ErrorPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import ProductsPage from "./ProductsPage";
+import AboutPage from "./AboutPage";
+import CartPage from "./CartPage";
+import CheckoutPage from "./CheckoutPage";
+import SingleProductPage from "./SingleProductPage";
+import ErrorPage from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  ProductsPage,
+  AboutPage,
+  CartPage,
+  CheckoutPage,
+  SingleProductPage,
+  ErrorPage,
+  PrivateRoute,
+};
